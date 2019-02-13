d4805e2a6bbbf2733fd818c611079a59f359b659
LannyWang009 <09legend.wang@gmail.com>
2019-02-12 21:10:07 -0600

    use immutable functions for rendering

--- circles/index.js
+++ circles/index.js
@@ -1,9 +1,34 @@
-const { createStore } = Redux;
-const { subscribe, dispatch, getState } = createStore(reducer);
+// ============================
+// util
+const deepCopy = (x) => JSON.parse(JSON.stringify(x))
 
-subscribe(() => render(getState()));
+// ==========================
+// Create Redux Store
 
-dispatch({ type: null }); // Here we're making a call to dispatch() - this triggers the first render.
+const { createStore } = Redux
+// Initialize the Redux store by passing it to our reducer
+const { subscribe, dispatch, getState } = createStore(reducer)
 
-// Write DOM event listeners here, make them dispatch actions to the Redux store 
+// pass the redux state to our render mehtod
+subscribe(() => render(getState().toJS()))
 
+// // Here we're making a call to dispatch() - this triggers the first render.
+// dispatch({ type: null })
+
+// ========================
+// DOM EVENT
+// const addCircleButton = document.getElementById('addCircle')
+// addCircleButton.addEventListener('click', addEvents)
+
+// Write DOM event listeners here, make them dispatch actions to the Redux store
+const addEvents = () => {
+  // Dispatch the "ADDCIRCLE" action every time the body is clicked
+  const addCircleButton = document.getElementById('addCircle')
+  addCircleButton.addEventListener('click', e => {
+    dispatch(
+      { type: 'ADDCIRCLE', radius: Math.floor((Math.random() * 100) + 10), color: Math.floor(Math.random() * 16777215).toString(16) }
+    )
+  })
+}
+
+addEvents()
--- circles/reducer.js
+++ circles/reducer.js
@@ -1,27 +1,38 @@
-const initialState = [
-    {
-        radius: 50,
-        color: "#FF00FF"
-    },
-    {
-        radius: 30,
-        color: "#FF99AA"
-    },
-    {
-        radius: 60,
-        color: "#0000FF"
-    },
-    {
-        radius: 10,
-        color: "#000000"
-    },
-];
+const initialState = Immutable.fromJS([
+  {
+    radius: 50,
+    color: '#FF00FF'
+  },
+  {
+    radius: 30,
+    color: '#FF99AA'
+  },
+  {
+    radius: 60,
+    color: '#0000FF'
+  },
+  {
+    radius: 10,
+    color: '#000000'
+  }
+])
 
 const reducer = (state = initialState, action) => {
-    // Handle actions here - make sure you don't mutate the state!
-    const { type } = action;
+  // Handle actions here - make sure you don't mutate the state!
+  // an error message in case the assertion is false
+  console.assert(Immutable.isImmutable(state), 'Redux store should be an immutable value')
+  let newState
+  const { type } = action
+  if (type === 'ADDCIRCLE') {
+    console.log('action', action)
+    var newCircle = {
+      radius: action.radius,
+      color: '#' + action.color
+    }
 
-    // ACTION: Add a random circle
+    newState = Immutable.set(state, state.toJS().length, newCircle)
+    console.log('new', newState.toJS())
+  }
 
-    return state;
+  return newState
 }
